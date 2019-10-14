5f6e014e203b66578bb6a0f0a27f3b10263bbd8d
teo lemane <tlemane@maratus.irisa.fr>
2019-10-14 13:51:55 +0200

    add example

---
+++ example/simple_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IN_DIR='./data'
+OUTPUT_DIR='./output_simka_howde'
+SIMKA_IN='./data/simka_input.txt'
+K=31
+MIN=1
+BF_SIZE=1000000
+
+python3 ../simka-HowDeSBT.py --in ${SIMKA_IN} --inDir ${IN_DIR} --k ${K} --abundance-min ${MIN} --bf-size ${BF_SIZE} --output-dir ${OUTPUT_DIR} --verbose
