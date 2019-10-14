22799fa97076a2e35a695aedc02666551283b8a5
teo lemane <tlemane@maratus.irisa.fr>
2019-10-14 15:59:12 +0200

    add link to output

--- example/simple_test.sh
+++ example/simple_test.sh
@@ -8,3 +8,5 @@ MIN=1
 BF_SIZE=1000000
 
 python3 ../simka-HowDeSBT.py --in ${SIMKA_IN} --inDir ${IN_DIR} --k ${K} --abundance-min ${MIN} --bf-size ${BF_SIZE} --output-dir ${OUTPUT_DIR} --verbose
+
+echo Done. Results in ${OUTPUT_DIR}, for details: https://github.com/TeoLem/Simka-HowDeSBT#output-directory
